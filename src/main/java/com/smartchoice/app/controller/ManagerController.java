6ea25081b76e49e33718c624230f2bc93b08709e
minsoopark <altntaos@nate.com>
2016-08-03 13:20:26 +0900

    merge 준비

--- src/main/java/com/smartchoice/app/controller/ManagerController.java
+++ src/main/java/com/smartchoice/app/controller/ManagerController.java
@@ -10,10 +10,8 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.junit.runners.Parameterized.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
